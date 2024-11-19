a3931578b13788f2d9a33ad7c5e3584c9156c3ba
Sudip Paul <p452570@gmail.com>
2024-11-19 21:14:23 +0530

    hellllllllo

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 import express from "express"
+import dotenv from "dotenv"
+import text from "./routes/text.js"
 
+dotenv.config()
 const app = express()
-const port = 9000
-
-app.get('/',(req,res) => {
-    // res.json({message:"Hello world"})
-    res.send("Hello world")
-})
+const port = process.env.PORT || 9000
+app.use('/text',text)
 
 app.listen(port,() => {
     console.log(`server running on ${port}`)
